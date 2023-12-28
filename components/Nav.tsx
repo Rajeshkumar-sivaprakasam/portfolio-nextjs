967212d446371e6fd04eaa4bdc5efe360cbc7fe7
rajesh <rk.sivaprakasam@outlook.com>
2023-12-28 16:42:38 +0530

    basic nav bar

---
+++ components/Nav.tsx
@@ -0,0 +1,31 @@
+import { Bars3Icon } from "@heroicons/react/16/solid";
+import React from "react";
+interface Props {
+  openNav: () => void;
+}
+const Nav = ({ openNav }: Props) => {
+  return (
+    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+      <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+        <h1 className="text-white flex-[0.6] cursor-pointer text-[25px] font-bold">
+          WEB
+          <span className="text-yellow-300">DEV</span>
+        </h1>
+        <div className="nav-link">Home</div>
+        <div className="nav-link">Services</div>
+        <div className="nav-link">About</div>
+        <div className="nav-link">Projects</div>
+        <div className="nav-link">Blog</div>
+        <div className="nav-link">Contact</div>
+        <div>
+          <Bars3Icon
+            className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300"
+            onClick={openNav}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Hero from "@/components/Hero";
+import MobileNav from "@/components/MobileNav";
+import Nav from "@/components/Nav";
+import React, { useState } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [nav, setNav] = useState(false);
+  const openNav = () => {
+    setNav(true);
+  };
+  const closeNav = () => {
+    setNav(false);
+  };
+
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        {/* NavBar section */}
+        <MobileNav closeNav={closeNav} nav={nav} />
+        <Nav openNav={openNav} />
+        {/* Hero Section */}
+        <Hero />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
